82e8ee5b098730f3bdf1026296681f2883115d15
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-24 13:21:39 -0500

    Add first const

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-  $name = 'Yoshi';
+
+  define('NAME', 'Yoshi');
+  // $name = 'Yoshi';
+  $age = 30;
 ?>
 
 <!DOCTYPE html>
@@ -12,5 +15,6 @@
   <body>
     <h1><?php echo 'hello, ninjas'; ?></h1>
     <div><?php   echo $name; ?></div>
+    <div><?php   echo $age; ?></div>
   </body>
 </html>
