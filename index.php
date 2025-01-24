d8bc4b7067ac178c90bc303197962efa59b71177
Alex Pitzel <C-Alexander.Pitzel@charter.com>
2025-01-24 12:30:07 -0500

    Add inital html template

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  echo 'hello world';
+  // echo 'hello world';
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      my first php file
+    </title>
+  </head>
+  <body>
+    <h1><?php echo 'hello, ninjas'; ?></h1>
+  </body>
+</html>
